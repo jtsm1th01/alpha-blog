7ff401a034a24a12c7c8c96c117c9511ad3e2edc
jtsm1th01 <mr_travis_smith@hotmail.com>
2016-09-15 16:51:16 +0000

    add article edit action and form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -15,6 +19,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
   
