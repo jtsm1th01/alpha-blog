475a68af1d9f6d250efc532c5b8f8f65a08a6415
jtsm1th01 <mr_travis_smith@hotmail.com>
2016-09-20 22:34:49 +0000

    add user edit and extract new/edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ def new
   @user = User.new
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
 def create
   @user = User.new(user_params)
   if @user.save
@@ -11,6 +15,16 @@ def create
     redirect_to articles_path
   else
     render 'new'
+  end
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:success] = "User was successfully updated."
+    redirect_to articles_path
+  else
+    render 'edit'
   end
 end
 
